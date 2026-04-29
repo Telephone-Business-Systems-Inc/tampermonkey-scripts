d0ab0a54d9913ea96b4bdf0c8572e420e50f7228
tbs-roberto-porfirio <roberto.porfirio@telbiz.com>
2026-04-29 13:39:59 -0400

    Create my-script.meta.js

---
+++ cripts/my-script.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         My Script
+// @namespace    https://github.com/YOUR_USERNAME/tampermonkey-scripts
+// @version      1.0
+// @description  What this script does
+// @author       Your Name
+// @match        https://example.com/*
+// @updateURL    https://raw.githubusercontent.com/YOUR_USERNAME/tampermonkey-scripts/main/scripts/my-script.meta.js
+// @downloadURL  https://raw.githubusercontent.com/YOUR_USERNAME/tampermonkey-scripts/main/scripts/my-script.user.js
+// @grant        none
+// ==/UserScript==
