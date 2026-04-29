501330326354f6a548840ca8045be8ead5d3edd2
tbs-roberto-porfirio <roberto.porfirio@telbiz.com>
2026-04-29 15:34:07 -0400

    Create odoo-chatter-drawer.meta.js

---
+++ scripts/odoo-chatter-drawer.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Odoo: Chatter Drawer Management
+// @namespace    https://telephone-business-systems-inc.odoo.com/odoo
+// @version      1.0.0
+// @description  control the display of the Odoo chatter portion of the screen when reviewing detailed records
+// @author       Roberto PORFIRIO
+// @match        https://*..odoo.com/odoo/*
+// @updateURL    
+// @downloadURL  
+// @grant        none
+// ==/UserScript== 
