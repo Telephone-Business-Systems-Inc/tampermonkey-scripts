3c77afbb90b0463e42c89299fe7071662063f6f5
tbs-roberto-porfirio <roberto.porfirio@telbiz.com>
2026-04-29 14:32:24 -0400

    Update my-script.user.js

--- scripts/my-script.user.js
+++ scripts/my-script.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         My Script
 // @namespace    https://github.com/YOUR_USERNAME/tampermonkey-scripts
-// @version      1.0.2
+// @version      1.0.3
 // @description  What this script does
-// @author       Your Name
+// @author       Roberto PORFIRIO
 // @match        https://example.com/*
 // @updateURL    https://raw.githubusercontent.com/YOUR_USERNAME/tampermonkey-scripts/main/scripts/my-script.meta.js
 // @downloadURL  https://raw.githubusercontent.com/YOUR_USERNAME/tampermonkey-scripts/main/scripts/my-script.user.js
