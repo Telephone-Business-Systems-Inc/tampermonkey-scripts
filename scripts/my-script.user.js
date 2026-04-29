353f0cdf8533c31c9c1b0377cd049c026c477d84
tbs-roberto-porfirio <roberto.porfirio@telbiz.com>
2026-04-29 14:39:09 -0400

    Update my-script.user.js

--- scripts/my-script.user.js
+++ scripts/my-script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         My Script
 // @namespace    https://github.com/Telephone-Business-Systems-Inc/tampermonkey-scripts/
-// @version      1.0.3
+// @version      1.0.4
 // @description  What this script does
 // @author       Roberto PORFIRIO
 // @match        https://example.com/*
