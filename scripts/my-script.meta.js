ff60ac4c1ce3adb77e360307708f8aae7622e1e6
tbs-roberto-porfirio <roberto.porfirio@telbiz.com>
2026-04-29 14:37:41 -0400

    Update my-script.meta.js

--- scripts/my-script.meta.js
+++ scripts/my-script.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         My Script
-// @namespace    https://github.com/YOUR_USERNAME/tampermonkey-scripts
+// @namespace    https://github.com/Telephone-Business-Systems-Inc/tampermonkey-scripts/
 // @version      1.0.3
 // @description  What this script does
 // @author       Roberto PORFIRIO
 // @match        https://example.com/*
-// @updateURL    https://raw.githubusercontent.com/YOUR_USERNAME/tampermonkey-scripts/main/scripts/my-script.meta.js
-// @downloadURL  https://raw.githubusercontent.com/YOUR_USERNAME/tampermonkey-scripts/main/scripts/my-script.user.js
+// @updateURL    https://github.com/Telephone-Business-Systems-Inc/tampermonkey-scripts/raw/refs/heads/main/scripts/my-script.meta.js
+// @downloadURL  https://github.com/Telephone-Business-Systems-Inc/tampermonkey-scripts/raw/refs/heads/main/scripts/my-script.meta.js
 // @grant        none
 // ==/UserScript== 
